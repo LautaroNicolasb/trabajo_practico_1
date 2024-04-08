e0aa86a217cb50b0236963fa74acd6940271b88f
NET <NET@DESKTOP-FFDDU8Q>
2024-04-08 19:23:47 -0300

    se agrego algo

--- tp1_BarrientosLautaro/src/ar/edu/unju/fi/ejercico2/Main.java
+++ tp1_BarrientosLautaro/src/ar/edu/unju/fi/ejercico2/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// Desarrollo del ejercicio 2
 		System.out.println("Ejercicio 2");
-
+		System.out.println("Hola Mundo");
+		
 	}
 
 }
