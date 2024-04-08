17309cd98b6fddd20a55b9fb23c03fe151afa7a5
NET <NET@DESKTOP-FFDDU8Q>
2024-04-08 19:20:18 -0300

    Se agrego paquete y classe para el ejercicio 2

---
+++ tp1_BarrientosLautaro/src/ar/edu/unju/fi/ejercico2/Main.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi.ejercico2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Desarrollo del ejercicio 2
+		System.out.println("Ejercicio 2");
+
+	}
+
+}
