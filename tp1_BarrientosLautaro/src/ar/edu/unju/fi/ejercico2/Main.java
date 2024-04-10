7d4b3fcf2f7123831f2dcc18783d50e3bc658617
NET <NET@DESKTOP-FFDDU8Q>
2024-04-09 21:48:40 -0300

    Se carga el ejercicio 2

--- tp1_BarrientosLautaro/src/ar/edu/unju/fi/ejercico2/Main.java
+++ tp1_BarrientosLautaro/src/ar/edu/unju/fi/ejercico2/Main.java
@@ -3,9 +3,20 @@ package ar.edu.unju.fi.ejercico2;
 public class Main {
 
 	public static void main(String[] args) {
-		// Desarrollo del ejercicio 2
-		System.out.println("Ejercicio 2");
-		System.out.println("Hola Mundo");
+		//Declarar variables
+		String nombrePais = "Argentina";
+		int edad = 20;
+		double alturaEdificio = 400;
+		double precioProducto = 1500;
+		String numeroTelefono = "3644369786";
+		double coseno = Math.cos(0.5);
+		//Mostrar los valores de cada variable
+		System.out.println("Nombre del país: "+nombrePais);
+		System.out.println("Edad: "+edad);
+		System.out.println("Altura del edificio: "+alturaEdificio);
+		System.out.println("Precio del preducto: "+precioProducto);
+		System.out.println("Número de teléfono: "+numeroTelefono);
+		System.out.println("Cálculo de coseno: "+coseno);
 		
 	}
 
