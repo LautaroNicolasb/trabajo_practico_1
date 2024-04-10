c8818815559cc66112116c751db38f5322a7c4c7
NET <NET@DESKTOP-FFDDU8Q>
2024-04-09 21:32:53 -0300

    Se realizo el ejercicio 1

--- tp1_BarrientosLautaro/src/ar/edu/unju/fi/ejercico1/Main.java
+++ tp1_BarrientosLautaro/src/ar/edu/unju/fi/ejercico1/Main.java
@@ -3,8 +3,15 @@ package ar.edu.unju.fi.ejercico1;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Bienvenido a Programación Visual");
+		//Declaración e inicialización de variables
+		int n1 = 5, n2= 8, n3 = 1, n4 =  20, n5 = 9;
+		//Calcular promedio 
+		double promedio = (n1 + n2 + n3 + n4 + n5)/5.0;
+		//mostrar el resultado del promedio
+		System.out.println("El primedio es: "+promedio);
+		
 		
 	}
+		
     
 }
