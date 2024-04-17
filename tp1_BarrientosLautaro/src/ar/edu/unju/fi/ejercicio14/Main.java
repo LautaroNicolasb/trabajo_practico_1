205672e5356feedef5bab9e9ddd385addd687b05
NET <NET@DESKTOP-FFDDU8Q>
2024-04-17 20:58:20 -0300

    se carga ejercicio 14

--- tp1_BarrientosLautaro/src/ar/edu/unju/fi/ejercicio14/Main.java
+++ tp1_BarrientosLautaro/src/ar/edu/unju/fi/ejercicio14/Main.java
@@ -1,10 +1,37 @@
 package ar.edu.unju.fi.ejercicio14;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese un número entre 3 - 10 : ");
+		int numero = scanner.nextInt();
+		scanner.nextLine();
+		int suma = 0;
+		int [] listaArray = new int[numero];
+		
+		if (numero >= 3 && numero <= 10) {
+			
+			for (int i=0;i<numero;i++) {
+				System.out.println("Ingrese un nombre "+(i+1)+": ");
+				listaArray[i] = scanner.nextInt();
+			}
+			for(Integer lista : listaArray) {
+				System.out.println(lista);
+				suma+= lista;
+			}
+			
+			System.out.println("Suma de los valores de la lista: "+suma);
+		
+		}else {
+		System.out.println("Ingreso no válido");
 	}
+		scanner.close();
+	}
+		
+		
+	
 
 }
